608494ae95e60ad495ff46908df56c85ab01f0e8
M Ridho Tanjung <muhammadridhot@gmail.com>
2019-12-11 11:47:23 +0700

    Add File Kondisi

---
+++ kondisi/kondisi.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	angka := 10
+	convertAngkaToString := fmt.Sprintln(angka) // Convert Int To String
+	// Convert float To int int(10.0) -> Menjadi int
+	fmt.Print(convertAngkaToString)
+
+	a, b := 20, 13
+	fmt.Println(a, b)
+	fmt.Printf("value anda adalah %v %v %9.f \n", a, b, 22421312312.2422)
+
+	c := 5
+	if c < 10 && c > 10 || c <= 10 {
+		fmt.Println("Oke")
+	}
+}
